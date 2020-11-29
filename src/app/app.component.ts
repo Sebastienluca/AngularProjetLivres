f565cc54165eda8c1057ce4f89f4c02900d33400
sebastienluca <webmaster@start-web.fr>
2020-11-29 15:52:52 +0100

    installation firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularProjetLivres';
+
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyBHpJpCSXkG2KVq021i4KPyPCW2TiwST0U",
+	  authDomain: "angularprojetlivres.firebaseapp.com",
+	  databaseURL: "https://angularprojetlivres.firebaseio.com",
+	  projectId: "angularprojetlivres",
+	  storageBucket: "angularprojetlivres.appspot.com",
+	  messagingSenderId: "661672157775",
+	  appId: "1:661672157775:web:de6b2fa92124280df9c03a",
+	  measurementId: "G-TYB2EGS58Q"
+    };
+    firebase.initializeApp(config);
+  }
 }
